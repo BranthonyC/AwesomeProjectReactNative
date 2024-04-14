27f969ae24e9c4756805721a9f2967def3b0497a
Brandon Anthony <brandon.chitay@ubiquo.io>
2024-04-14 11:02:43 -0600

    feature/ReduxStateManagement

---
+++ src/states/reducers/counterReducer.js
@@ -0,0 +1,15 @@
+// reducers.js
+import initialState from '../states/counterState';
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {...state, counter: state.counter + 1};
+    case 'DECREMENT':
+      return {...state, counter: state.counter - 1};
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
